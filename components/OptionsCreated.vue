c5cb2a6da7f70c9305cfb50ac9deadfa30081767
i.kyvatskyi <i.kyatskyi.dev@gmail.com>
2025-04-26 19:40:46 +0300

    Added Options API example

---
+++ components/OptionsCreated.vue
@@ -0,0 +1,20 @@
+<template>
+  <p>Hello</p>
+</template>
+
+<script>
+export default {
+  methods: {
+    handleResize(){
+      console.log('aloha')
+    }
+  },
+  created(){
+    this.$eventBus.on('resize', this.handleResize)
+  },
+  beforeUnmount(){
+
+    this.$eventBus.off('resize', this.handleResize)
+  }
+}
+</script>
--- pages/post/[id].vue
+++ pages/post/[id].vue
@@ -1,9 +1,11 @@
 <template>
   <h2>Post {{ route.params.id }}</h2>
+  <OptionsCreated />
 </template>
 
 <script setup>
 import { useRoute } from 'vue-router';
+import OptionsCreated from '@/components/OptionsCreated.vue';
 
 const route = useRoute();
 
